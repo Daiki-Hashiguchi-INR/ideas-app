8632cd91b1bf4079375f5fa52be206ad22981545
hassi-INR <hassi.d.cat@gmail.com>
2021-03-01 15:23:40 +0900

    カテゴリー欄の作成

---
+++ app/controllers/categories_controller.rb
@@ -0,0 +1,19 @@
+class CategoriesController < ApplicationController
+  def index
+    @categories = Category.all
+  end
+
+  def new
+    @category = Category.new
+  end
+
+  def create
+    @category = Category.new(category_params)
+  end
+end
+
+private
+
+def category_params
+  
+end
